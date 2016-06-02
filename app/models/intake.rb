d0069ad0b92851c30222c4b69e341cce58b8b2be
lukephelan <luke.phelan@hotmail.com>
2016-06-02 14:23:57 +1000

    Proximate value is now releated to the quantity consumed

--- app/models/intake.rb
+++ app/models/intake.rb
@@ -16,13 +16,13 @@ class Intake < ActiveRecord::Base
       @proximate = Proximate.new
       @api_response = HTTParty.get "http://api.nal.usda.gov/ndb/reports/?ndbno=#{self.ndbno}&type=b&format=json&api_key=8gYd9RFbST30DyUJm7pJ0Q2Rbjsv9fseOAKe2O6K"
       @proximate.intake_id = self.id
-      @proximate.water_g = @api_response["report"]["food"]["nutrients"][0]["value"]
-      @proximate.energy_kcal = @api_response["report"]["food"]["nutrients"][1]["value"]
-      @proximate.protein_g = @api_response["report"]["food"]["nutrients"][2]["value"]
-      @proximate.total_fat_g = @api_response["report"]["food"]["nutrients"][3]["value"]
-      @proximate.carbohydrate_g = @api_response["report"]["food"]["nutrients"][4]["value"]
-      @proximate.total_dietary_fibre_g = @api_response["report"]["food"]["nutrients"][5]["value"]
-      @proximate.total_sugar_g = @api_response["report"]["food"]["nutrients"][6]["value"]
+      @proximate.water_g = (@api_response["report"]["food"]["nutrients"][0]["value"].to_f)*(self.consumed_qty.to_f/100)
+      @proximate.energy_kcal = (@api_response["report"]["food"]["nutrients"][1]["value"].to_f)*(self.consumed_qty.to_f/100)
+      @proximate.protein_g = (@api_response["report"]["food"]["nutrients"][2]["value"].to_f)*(self.consumed_qty.to_f/100)
+      @proximate.total_fat_g = (@api_response["report"]["food"]["nutrients"][3]["value"].to_f)*(self.consumed_qty.to_f/100)
+      @proximate.carbohydrate_g = (@api_response["report"]["food"]["nutrients"][4]["value"].to_f)*(self.consumed_qty.to_f/100)
+      @proximate.total_dietary_fibre_g = (@api_response["report"]["food"]["nutrients"][5]["value"].to_f)*(self.consumed_qty.to_f/100)
+      @proximate.total_sugar_g = (@api_response["report"]["food"]["nutrients"][6]["value"].to_f)*(self.consumed_qty.to_f/100)
       @proximate.save
     end
 
